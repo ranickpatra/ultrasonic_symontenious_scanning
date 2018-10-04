6cc66267955fcba7f4f3edae7ee8bc018a936850
Ranick Patra <ranickpatra.nalikul@gmail.com>
2018-10-04 20:35:42 +0530

    added serial for testing

--- read_usn/read_usn.ino
+++ read_usn/read_usn.ino
@@ -15,8 +15,9 @@ unsigned long current_time;
 
 void setup() {
 
+	Serial.begin(115200);
 	Wire.begin(0x05);						// begin I2C as slave on 0x05
-	TWBR = 12;								// set SCL at 400KHz
+	//TWBR = 12;								// set SCL at 400KHz
  	DDRD |= B11110000;						// PD4, PD5, PD6, PD7 as output
 	DDRB &= B11110000;						// PB0, PB1, PB2, PB3 as input
 	DDRC |= B00000001;						// PC0 as output
@@ -27,6 +28,8 @@ void setup() {
 	PCMSK0 |= B00001111;					// to enable satate change interrupt
 
 	Wire.onRequest(requestEvent);
+	pinMode(13, OUTPUT);
+	digitalWrite(13, LOW);
 
 }
 
@@ -52,6 +55,19 @@ void loop() {
 		}
 	}
 
+	now = micros() + 50000;
+	while(now > micros());
+	digitalWrite(13, HIGH);
+	now = micros() + 50000;
+	while(now > micros());
+	digitalWrite(13, LOW);
+	Serial.print(cm[0]); Serial.print(", ");
+	Serial.print(cm[1]); Serial.print(", ");
+	Serial.print(cm[2]); Serial.print(", ");
+	Serial.println(cm[3]);
+
+
+
 }
 
 
@@ -64,7 +80,7 @@ ISR(PCINT0_vect) {
 
 	current_time = micros() - now;					//  store the current_time in microseconds
 
-	if(PINB & B00000001) {						// check is PB0 is high or not
+	if(PINB & B00000001 && !changed[0]) {						// check is PB0 is high or not
 		changed[0] = true;
 		if(current_time < THRESHOLD) {
 			pulse_time[0] = current_time;
@@ -73,7 +89,7 @@ ISR(PCINT0_vect) {
 		}
 	}
 
-	if(PINB & B00000010) {						// check is PB1 is high or not
+	if(PINB & B00000010 && !changed[1]) {						// check is PB1 is high or not
 		changed[1] = true;
 		if(current_time < THRESHOLD) {
 			pulse_time[1] = current_time;
@@ -82,7 +98,7 @@ ISR(PCINT0_vect) {
 		}
 	}
 
-	if(PINB & B00000100) {						// check is PB2 is high or not
+	if(PINB & B00000100 && !changed[2]) {						// check is PB2 is high or not
 		changed[2] = true;
 		if(current_time < THRESHOLD) {
 			pulse_time[2] = current_time;
@@ -91,7 +107,7 @@ ISR(PCINT0_vect) {
 		}
 	}
 
-	if(PINB & B00001000) {						// check is PB3 is high or not
+	if(PINB & B00001000 && !changed[3]) {						// check is PB3 is high or not
 		changed[3] = true;
 		if(current_time < THRESHOLD) {
 			pulse_time[3] = current_time;
