25abd7c61b9e646f392ea870c4e54e5b16fe8849
Ranick Patra <ranickpatra.nalikul@gmail.com>
2018-10-13 15:07:26 +0530

    complete using traditional; method

--- read_usn/read_usn.ino
+++ read_usn/read_usn.ino
@@ -5,13 +5,12 @@
 #include <Wire.h>
 
 
-const unsigned long THRESHOLD = 24000;
 
-int cm[4] = {0, 0, 0, 0};
+uint16_t cm[4] = {0, 0, 0, 0};
 
 void setup() {
 
-	Serial.begin(115200);
+
 	Wire.begin(0x05);						// begin I2C as slave on 0x05
 	//TWBR = 12;								// set SCL at 400KHz
  	DDRD |= B11110000;						// PD4, PD5, PD6, PD7 as output
@@ -39,14 +38,8 @@ void loop() {
 		PORTD |= 1 << (4+i);
 		delayMicroseconds(10);
 		PORTD &= B00001111;
-		cm[i] = to_cm(pulseIn(i + 8, HIGH, 11900) / 2);
+		cm[i] = to_cm(pulseIn(i + (1 << 3), HIGH, 11900) / 2);
 	}
-
-	Serial.print(cm[0]); Serial.print(", ");
-	Serial.print(cm[1]); Serial.print(", ");
-	Serial.print(cm[2]); Serial.print(", ");
-	Serial.println(cm[3]);
-
 
 	delay(50);
 
@@ -54,65 +47,18 @@ void loop() {
 }
 
 
-int to_cm(unsigned long m_time) {
-	return (int) (m_time * 0.0335);
+uint16_t to_cm(unsigned long m_time) {
+	return (uint16_t) (m_time * 0.0335);
 }
-
-
-// // The interrupt service routine (ISR)
-// ISR(PCINT0_vect) {
-//
-// 	if(~PINB & B00001111 == B00001111)			// on interrupt if pin is not high
-// 		return;
-//
-// 	current_time = micros() - now;					//  store the current_time in microseconds
-//
-// 	if(PINB & B00000001 && !changed[0]) {						// check is PB0 is high or not
-// 		changed[0] = true;
-// 		if(current_time < THRESHOLD) {
-// 			pulse_time[0] = current_time;
-// 		} else {
-// 			pulse_time[0] = 0;
-// 		}
-// 	}
-//
-// 	if(PINB & B00000010 && !changed[1]) {						// check is PB1 is high or not
-// 		changed[1] = true;
-// 		if(current_time < THRESHOLD) {
-// 			pulse_time[1] = current_time;
-// 		} else {
-// 			pulse_time[1] = 0;
-// 		}
-// 	}
-//
-// 	if(PINB & B00000100 && !changed[2]) {						// check is PB2 is high or not
-// 		changed[2] = true;
-// 		if(current_time < THRESHOLD) {
-// 			pulse_time[2] = current_time;
-// 		} else {
-// 			pulse_time[2] = 0;
-// 		}
-// 	}
-//
-// 	if(PINB & B00001000 && !changed[3]) {						// check is PB3 is high or not
-// 		changed[3] = true;
-// 		if(current_time < THRESHOLD) {
-// 			pulse_time[3] = current_time;
-// 		} else {
-// 			pulse_time[3] = 0;
-// 		}
-// 	}
-//
-// }
 
 
 // function that executes whenever data is requested by master
 void requestEvent() {
-
 	//4 * sizeof(int) = 4 * 2 = 8 bytes of data
 
 	for(int i=0; i < 4; i++) {
-		Wire.write(cm[i]);
+		Wire.write((cm[i] >> 8) & 0xFF);
+		Wire.write(cm[i] & 0xFF);
 	}
 
 }
